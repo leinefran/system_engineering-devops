937f6548232cb97769f7327c51ef050bab93745e
leinefran <penny.leine@gmail.com>
2019-03-08 17:29:31 +0000

    reconfiguring the web server setup to better handle requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,15 @@
-# reconfiguring the web server setup to better handle too many requests
+# reconfiguring the web server setup to better handle requests
+
+service { 'nginx' :
+  ensure => 'running',
+  enable => true
+}
+
+file {'/etc/default/nginx' :
+  ensure => present
+}
+-> exec { 'change_ulimit' :
+  path    => '/bin',
+  command => 'sed -i --follow-symlinks s/15/1000/ /etc/default/nginx',
+  notify  => Service['nginx']
+}
